83222b97a5899d9f6a26226ea8097026feff714b
danisacramento <danielleacsacramento@gmail.com>
2020-01-05 22:33:27 -0300

    Primeiro commit do projeto de e-commerce

---
+++ mirror-fashion/js/home.js
@@ -0,0 +1,30 @@
+
+document.querySelector('#form-busca').onsubmit = function(){
+    if (document.querySelector('#q').value == '') {
+        document.querySelector('#form-busca').style.background='red';
+        return false; 
+    }
+};
+//banner rotativo//
+var banners=["img/destaque-home.png", "img/destaque-home-2.png"];
+var bannerAtual = 0;
+
+function trocaBanner(){
+    bannerAtual = (bannerAtual +1)%2;
+    document.querySelector('.destaque img').src =banners[bannerAtual];
+}
+var timer = setInterval(trocaBanner, 4000);
+
+/*var controle = document.querySelector('pause');
+
+    controle.onclick = function() {
+        if (controle.className =='pause'){
+            clearInterval(timer);
+            controle.className = 'play';
+        } else{
+            timer = setInterval(trocaBanner, 4000);
+            controle.className ='pause';
+        }
+    return false;
+};
+*/
